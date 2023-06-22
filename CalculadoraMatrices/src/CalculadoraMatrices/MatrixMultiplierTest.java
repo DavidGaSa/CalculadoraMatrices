bd8ff210877fb9c8c427e06ee5d7a6e698c27202
David García Salgado <131993697+DavidGaSa@users.noreply.github.com>
2023-06-22 13:27:27 +0200

    Add files via upload

---
+++ CalculadoraMatrices/src/CalculadoraMatrices/MatrixMultiplierTest.java
@@ -0,0 +1,30 @@
+package CalculadoraMatrices;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+ public class MatrixMultiplierTest {
+    @Test
+    public void testMultiply() {
+        int[][] matrix1 = {
+            { 1, 2, },
+            { 3, 4, }
+        };
+
+        int[][] matrix2 = {
+            { 5, 6, },
+            { 7, 8, }
+        };
+
+        int[][] expected = {
+            { 19, 22 },
+            { 43, 50 }
+        };
+
+        int[][] result = MatrixMultiplier.multiplyMatrices(matrix1, matrix2);
+
+        assertArrayEquals(expected,result);
+    }
+		
+	}
---
+++ CalculadoraMatrices/src/module-info.java
@@ -0,0 +1,3 @@
+module CalculadoraMatrices {
+	requires junit;
+}
